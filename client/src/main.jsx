16f06d052a02eb252efc52c06d17aca63ed24d23
mkkeems <mkkeems@gmail.com>
2023-11-25 15:47:57 +0900

    creating basic routes and pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HomePage, SearchResultsPage, ClinicProfilePage } from "./pages";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/results",
+    element: <SearchResultsPage />,
+  },
+  {
+    path: "/clinic-profile/:clinicId",
+    element: <ClinicProfilePage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ client/src/pages/index.js
@@ -0,0 +1,5 @@
+import HomePage from "./HomePage";
+import SearchResultsPage from "./SearchResultsPage";
+import ClinicProfilePage from "./ClinicProfilePage";
+
+export { HomePage, SearchResultsPage, ClinicProfilePage };
