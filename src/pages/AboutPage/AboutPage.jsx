5a5aa6268f4811bcf22ca567c5e4609f8cd67fc5
mkkeems <mkkeems@gmail.com>
2023-11-26 11:27:35 +0900

    adding gif for about page

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
-
+import { SectionContent } from '../HomePage/styles';
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <SectionContent>
+      <h1>Hello! We are the BugBusters!🐛🐛🐛🐛</h1>
+      <iframe
+        src="https://giphy.com/embed/7YC00WP2WxRsTRPAbJ"
+        width="480"
+        height="315"
+        // frameBorder="0"
+        // class="giphy-embed"
+        allowFullScreen
+      ></iframe>
+
+      <h2>This app is created by</h2>
+      <h3>Elizbeth</h3>
+      <h3>Kevin</h3>
+      <h3>Sarah</h3>
+      <h3>Suzie</h3>
+      <h3>Michelle</h3>
+    </SectionContent>
+  );
 };
 
 export default AboutPage;
